5ff8a1d21b7d0a63884c6b1de3512c19ead03fc7
natiexperiencia <natiexperiencia@gmail.com>
2015-11-04 08:28:49 +0100

    repaso 2

--- segundoDW32/Ariketak/Repaso/2/funciones.js
+++ segundoDW32/Ariketak/Repaso/2/funciones.js
@@ -2,6 +2,10 @@ function cambia (letra,source) {
 	var cachos = source.split("/");
 	var src = cachos.pop();
 	if (src != "Vacio.gif") {
-		document.getElementById("z").src = "../../../resources/Letras/Vacio.gif"
-	};
+		document.getElementById(letra).src = "../../../resources/Letras/Vacio.gif";
+	}else if(letra == "i2"){
+		document.getElementById("i2").src = "../../../resources/Letras/I.gif";
+	}else{
+		document.getElementById(letra).src = "../../../resources/Letras/"+letra.toUpperCase()+".gif";
+	}
 }
