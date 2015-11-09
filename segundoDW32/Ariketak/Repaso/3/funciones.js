eb90744b7644c5d7ebe3e6cce1c2919888888898
natiexperiencia <natiexperiencia@gmail.com>
2015-11-09 09:59:26 +0100

    repaso 4

---
+++ segundoDW32/Ariketak/Repaso/3/funciones.js
@@ -0,0 +1,30 @@
+var foto = 1;
+function siguiente () {
+	if (foto == 8) {
+		document.getElementById('galeria').src="galeria/1.jpg";
+		foto = 1;
+	}else{
+		foto += 1;
+		document.getElementById('galeria').src="galeria/"+foto+".jpg";
+	}
+}
+
+function anterior () {
+	if (foto == 1) {
+		document.getElementById('galeria').src="galeria/8.jpg";
+		foto = 8;
+	}else{
+		foto -= 1;
+		document.getElementById('galeria').src="galeria/"+foto+".jpg";
+	}
+}
+
+function encima (boton) {
+	boton.style.backgroundColor = "red";
+	boton.style.color="white";
+}
+
+function fuera (boton) {
+	boton.style.backgroundColor="buttonface";
+	boton.style.color="black";
+}
