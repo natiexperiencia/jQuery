5345436b5239137abc9c5e249ec8cfa3ffe189ff
natiexperiencia <natiexperiencia@gmail.com>
2015-11-25 19:22:45 +0100

    no entra en success

--- segundoDW32/Ariketak/2ª Eval/2-Ejercicio Cookies Ajax/funciones.js
+++ segundoDW32/Ariketak/2ª Eval/2-Ejercicio Cookies Ajax/funciones.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-	
+
 	$.ajax({
 		url: 'datos.php',
 		type: 'post',
 		dataType: 'json',
-	})
-	succes: function (data) {
-		var resp = "<thead><tr><th>Descripción</th><th>Precio</th></tr></thead>";
-		for (var i = 0;i<data.length; i++) {
-			resp += "<tr><td>";
-			
-		};
-	}
 	
+		succes: function (data) {
+			//var resp = "<table><thead><tr><th>Descripción</th><th>Precio</th></tr></thead></table>";
+			/*for (var i = 0;i<data.length; i++) {
+				resp += "<tr><td>";
+				resp += data[i].descripcion+"</td><td>"+data[i].dolares+"</td></tr>";
+			};*/
+			console.log(data);
+			//$('#tabla').html("<p>helo</p>");
+		}
+	})
+	.done(function() {
+		console.log("done");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
 });
